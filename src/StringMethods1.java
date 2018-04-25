33bec14d2c535b0fade2b6e8bc238e07a2a9c997
Dewitt.Max <Dewitt.Max@SEMS-259-09.SEMS259.LAB>
2018-04-25 14:57:38 -0700

    PiAloud, StringMethods, GoofyNames

--- src/StringMethods1.java
+++ src/StringMethods1.java
@@ -7,16 +7,18 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+        String string = "strings";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(string.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length
-
+		System.out.println(string.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+        for (int i = 0; i < string.length(); i++) {
+		 System.out.println(string.charAt(i));   
+		}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
@@ -24,6 +26,13 @@ public class StringMethods1 {
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
 		
+        for (int z = 0; z < string.length(); z++) {
+        	char chars = string.charAt(z);
+			if (chars == 't') {
+				System.out.println("t is at index " + z);
+			}
+		}
+        
 	}
 }
 
